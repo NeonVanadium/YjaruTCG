f751cfaec3bad24bb8258cc5498f44a1dfe59e22
NeonVanadium <jackwhitten@outlook.com>
2018-05-11 23:34:14 -0700

    Friday
    some work

--- YjaruTCG/DeckManager.cs
+++ YjaruTCG/DeckManager.cs
@@ -15,7 +15,10 @@ namespace YjaruTCG
 
             ObservableCollection<Card> deck = new ObservableCollection<Card>();
 
-            deck.Add(new Card { name = "William Mayden", cost = 3, type = "Troop", legendary=true, color="Green", text = "The Harrondian Archaeologist can never quite seem to escape involvement in world-changing conflict." } );
+            deck.Add(new Card { name = "William Mayden", cost = 3, type = "Character", legendary=true, color="Green", text = "The Harrondian Archaeologist can never quite seem to escape involvement in world-changing conflict." } );
+            deck.Add(new Card { name = "Gael, the Unkillable", cost = 5, type = "Character", legendary = true, color = "DarkBlue", text = "A combination of terrible luck, skill, and unwavering resilience led to this particular university mage becoming one of the most renowned fighters in the world." });
+            deck.Add(new Card { name = "Greater Shard", cost = 4, type = "Item", legendary = true, color = "Red", text = "One of the three greater shards of the creator." });
+
 
             return deck;
 
