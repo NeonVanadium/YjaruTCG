47feba874aea8ffbf610b332af150b8f8e5cbeba
NeonVanadium <jackwhitten@outlook.com>
2018-05-14 22:13:56 -0700

    for school
    pushing progress

--- YjaruTCG/Card.cs
+++ YjaruTCG/Card.cs
@@ -8,11 +8,16 @@ namespace YjaruTCG
 {
     class Card
     {
+        public int width = 200;
+        public int height = 280;
+
         public string name { get; set; }
         public string color { get; set; }
         public string type { get; set; }
         public string text { get; set; }
         public int cost { get; set; }
+        public int attack { get; set; }
+        public int health { get; set; }
         public Boolean legendary { get; set; }
     }
 }
--- YjaruTCG/DeckManager.cs
+++ YjaruTCG/DeckManager.cs
@@ -15,9 +15,9 @@ namespace YjaruTCG
 
             ObservableCollection<Card> deck = new ObservableCollection<Card>();
 
-            deck.Add(new Card { name = "William Mayden", cost = 3, type = "Character", legendary=true, color="Green", text = "The Harrondian Archaeologist can never quite seem to escape involvement in world-changing conflict." } );
-            deck.Add(new Card { name = "Gael, the Unkillable", cost = 5, type = "Character", legendary = true, color = "DarkBlue", text = "A combination of terrible luck, skill, and unwavering resilience led to this particular university mage becoming one of the most renowned fighters in the world." });
-            deck.Add(new Card { name = "Greater Shard", cost = 4, type = "Item", legendary = true, color = "Red", text = "One of the three greater shards of the creator." });
+            deck.Add(new Card { name = "William Mayden", cost = 3, type = "Character", attack=4, health=5, legendary=true, color="Green", text = "The Harrondian Archaeologist can never quite seem to escape involvement in world-changing conflict." } );
+            deck.Add(new Card { name = "Gael, the Unkillable", cost = 5, type = "Character", attack = 4, health = 5, legendary = true, color = "Blue", text = "A combination of terrible luck, skill, and unwavering resilience led to this particular university mage becoming one of the most renowned fighters in the world." });
+            deck.Add(new Card { name = "Greater Shard", cost = 4, type = "Item", health = 2, legendary = true, color = "Red", text = "One of the three greater shards of the creator." });
 
 
             return deck;
